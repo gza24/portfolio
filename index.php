b8faf78d207099915f385fd347bf8c97a78f469b
garza4732 <garzalucas473@gmail.com>
2019-11-19 16:30:44 +0000

    My first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links Garza CSP 1</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <li><a href="Garza_AboutMe.html">AboutMe.html</a></li></br>
+      <li><a href="Garza_Projects.html">Projects.html</a></li></br>
+      <li><a href="Garza_Resume.html">Resume.html</a></li></br>
+      <li><a href="Garza_ContactMe.html">ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html>
